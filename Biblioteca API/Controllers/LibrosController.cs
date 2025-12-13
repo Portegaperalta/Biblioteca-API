4b292691416dc19dabe4f9d20a81b4a7551bc3cf
Pablo <ortegaperaltap@gmail.com>
2025-12-13 11:46:39 -0400

    Crear get request para obtener lista de todos los libros

---
+++ Biblioteca API/Controllers/LibrosController.cs
@@ -0,0 +1,25 @@
+﻿using Biblioteca_API.Datos;
+using Biblioteca_API.Entidades;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Biblioteca_API.Controllers
+{
+    [ApiController]
+    [Route("api/libros")]
+    public class LibrosController: ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        public LibrosController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        // GET: api/libros
+        [HttpGet]
+        public async Task<IEnumerable<Libro>> Get()
+        {
+            return await context.Libros.ToListAsync();
+        }
+    }
+}
