21c3efd2be2deb13d7103666fb050ff6f548c479
Pablo <ortegaperaltap@gmail.com>
2025-12-12 21:20:07 -0400

    Agregar EF Core y modificar AutoresController para que devuelva una lista hardcoded de autores

--- Biblioteca API/Controllers/AutoresController.cs
+++ Biblioteca API/Controllers/AutoresController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Biblioteca_API.Entidades;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Biblioteca_API.Controllers
 {
@@ -7,9 +8,13 @@ namespace Biblioteca_API.Controllers
     public class AutoresController: ControllerBase
     {
         [HttpGet]
-        public string Get()
+        public IEnumerable<Autor> Get()
         {
-            return "autores";
+            return new List<Autor>
+            {
+                new Autor{Id = 1,Nombre = "Pablo Neruda"},
+                new Autor{Id = 2, Nombre = "Ernest Hemingway"}
+            };
         }
     }
 }
