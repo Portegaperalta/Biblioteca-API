389f52964d5732bfc37515c3e611a15faf33967c
Pablo <ortegaperaltap@gmail.com>
2025-12-13 22:13:59 -0400

    agregar propiedad edad entre 18 y 20

--- Biblioteca API/Entidades/Autor.cs
+++ Biblioteca API/Entidades/Autor.cs
@@ -9,5 +9,8 @@ namespace Biblioteca_API.Entidades
         [StringLength(30,ErrorMessage ="El campo {0} debe tener {1} caracteres o menos")]
         public required string Nombre { get; set; }
         public List<Libro> Libros { get; set; } = new List<Libro>();
-    }
+
+        [Range(18,120)]
+        public int Edad { get; set; }
+        }
 }
