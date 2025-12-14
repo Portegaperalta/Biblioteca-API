3b05cdc0a959716aef92827276c799e7f8856240
Pablo <ortegaperaltap@gmail.com>
2025-12-13 22:04:13 -0400

    agregar mensaje de error personalizado a la propiedad nombre

--- Biblioteca API/Entidades/Autor.cs
+++ Biblioteca API/Entidades/Autor.cs
@@ -5,7 +5,7 @@ namespace Biblioteca_API.Entidades
     public class Autor
     {
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage ="El campo Nombre es obligatorio")]
         public required string Nombre { get; set; }
         public List<Libro> Libros { get; set; } = new List<Libro>();
     }
