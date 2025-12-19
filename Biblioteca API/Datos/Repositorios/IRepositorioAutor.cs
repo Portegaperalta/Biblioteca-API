4d5f28ec80aac91277ca4607c743b8c3af6bca95
Pablo <ortegaperaltap@gmail.com>
2025-12-19 16:25:09 -0400

    crear Repositorio Autor y agregar metodos Get

--- Biblioteca API/Datos/Repositorios/IRepositorioAutor.cs
+++ Biblioteca API/Datos/Repositorios/IRepositorioAutor.cs
@@ -5,7 +5,7 @@ namespace Biblioteca_API.Datos.Repositorios
     public interface IRepositorioAutor
     {
         Task<IEnumerable<Autor>> GetAutoresAsync();
-        Task<Autor> GetAutor(int id);
+        Task<Autor?> GetAutor(int id);
         Task<Autor> GetPrimerAutor();
         Task CreateAutor();
         Task UpdateAutor();
---
+++ Biblioteca API/Datos/Repositorios/RepositorioAutor.cs
@@ -0,0 +1,30 @@
+﻿using Biblioteca_API.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace Biblioteca_API.Datos.Repositorios
+{
+    public class RepositorioAutor : IRepositorioAutor
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RepositorioAutor(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Autor>> GetAutoresAsync()
+        {
+            return await _context.Autores.ToListAsync();
+        }
+
+        public async Task<Autor?> GetAutor(int id)
+        {
+            return await _context.Autores.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<Autor> GetPrimerAutor()
+        {
+            return await _context.Autores.FirstAsync();
+        }
+    }
+}
