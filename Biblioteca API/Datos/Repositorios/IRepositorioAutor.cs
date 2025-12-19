4de67756ba72c1263c0a6a2f7b408e7c811591c1
Pablo <ortegaperaltap@gmail.com>
2025-12-19 16:16:03 -0400

    crear IRepositorioAutor

---
+++ Biblioteca API/Datos/Repositorios/IRepositorioAutor.cs
@@ -0,0 +1,14 @@
+﻿using Biblioteca_API.Entidades;
+
+namespace Biblioteca_API.Datos.Repositorios
+{
+    public interface IRepositorioAutor
+    {
+        Task<IEnumerable<Autor>> GetAutoresAsync();
+        Task<Autor> GetAutor(int id);
+        Task<Autor> GetPrimerAutor();
+        Task CreateAutor();
+        Task UpdateAutor();
+        Task<int> DeleteAutor();
+    }
+}
