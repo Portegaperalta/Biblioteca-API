58eefaddd461e642d4317aa1e7b3a92936e126cd
Pablo <ortegaperaltap@gmail.com>
2025-12-20 12:40:31 -0400

    agregar DTO para creacion de autor

---
+++ Biblioteca API/DTOs/AutorCreacionDTO.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Biblioteca_API.Entidades;
+using Biblioteca_API.Validaciones;
+
+namespace Biblioteca_API.DTOs
+{
+    public class AutorCreacionDTO
+    {
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [StringLength(150, ErrorMessage = "El campo {0} debe tener {1} caracteres o menos")]
+        [PrimeraLetraMayuscula]
+        public required string Nombres { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [StringLength(150, ErrorMessage = "El campo {0} debe tener {1} caracteres o menos")]
+        [PrimeraLetraMayuscula]
+        public required string Apellidos { get; set; }
+
+        [StringLength(20, ErrorMessage = "El campo {0} debe tener {1} caracteres o menos")]
+        public string? Identificacion { get; set; }
+    }
+}
