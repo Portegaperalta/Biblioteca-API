3a40a03ae37b86ef6adefe60d32e7bbd9c0b8891
Pablo <ortegaperaltap@gmail.com>
2025-12-20 13:25:57 -0400

    agregar atributo NombreAutor

--- Biblioteca API/DTOs/LibroDTO.cs
+++ Biblioteca API/DTOs/LibroDTO.cs
@@ -5,8 +5,12 @@ namespace Biblioteca_API.DTOs
     public class LibroDTO
     {
         public int Id { get; set; }
+
         [Required]
         [StringLength(250, ErrorMessage = "El titulo del libro debe ser 250 caracteres o menos")]
         public required string Titulo { get; set; }
+        
+        [StringLength(250, ErrorMessage = "El nombre del autor debe ser 150 caracteres o menos")]
+        public required string NombreAutor { get; set; }
     }
 }
