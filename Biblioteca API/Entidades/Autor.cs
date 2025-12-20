aa192fedd87185be982b6af74af6eabab45c7f7a
Pablo <ortegaperaltap@gmail.com>
2025-12-19 22:39:58 -0400

    refactorizar Autor para guardar nombres en una columna y apellidos en otra

--- Biblioteca API/Entidades/Autor.cs
+++ Biblioteca API/Entidades/Autor.cs
@@ -6,10 +6,17 @@ namespace Biblioteca_API.Entidades
     public class Autor
     {
         public int Id { get; set; }
+
         [Required(ErrorMessage ="El campo {0} es obligatorio")]
         [StringLength(150,ErrorMessage ="El campo {0} debe tener {1} caracteres o menos")]
         [PrimeraLetraMayuscula]
-        public required string Nombre { get; set; }
+        public required string Nombres { get; set; }
+
+        [Required(ErrorMessage ="El campo {0} es obligatorio")]
+        [StringLength(150,ErrorMessage ="El campo {0} debe tener {1} caracteres o menos")]
+        [PrimeraLetraMayuscula]
+        public required string Apellidos { get; set; }
+
         public List<Libro> Libros { get; set; } = new List<Libro>();
         }
 }
