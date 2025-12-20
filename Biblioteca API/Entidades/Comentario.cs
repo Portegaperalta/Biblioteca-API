2c3ff3f0d9634cb1ccdcf77ea04b67ea862c524f
Pablo <ortegaperaltap@gmail.com>
2025-12-20 18:29:51 -0400

    crear modelo Comentario

---
+++ Biblioteca API/Entidades/Comentario.cs
@@ -0,0 +1,12 @@
+﻿namespace Biblioteca_API.Entidades
+{
+    public class Comentario
+    {
+        public Guid Id { get; set; }
+        public required string Autor { get; set; }
+        public required string Cuerpo { get; set; }
+        public DateTime FechaPublicacion { get; set; }
+        public int LibroId { get; set; }
+        public Libro? Libro { get; set; }
+    }
+}
