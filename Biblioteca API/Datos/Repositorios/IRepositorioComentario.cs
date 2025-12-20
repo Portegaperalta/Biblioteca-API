48b8d04255ade3d45e582e024f69943411b239d9
Pablo <ortegaperaltap@gmail.com>
2025-12-20 19:50:29 -0400

    crear Interfaz para repositorio Comentario

---
+++ Biblioteca API/Datos/Repositorios/IRepositorioComentario.cs
@@ -0,0 +1,13 @@
+﻿using Biblioteca_API.Entidades;
+
+namespace Biblioteca_API.Datos.Repositorios
+{
+    public interface IRepositorioComentario
+    {
+        Task<IEnumerable<Comentario>> GetComentariosAsync(int libroId);
+        Task<Comentario> GetComentarioAsync(int libroId, Guid comentarioId);
+        Task CreateComentarioAsync(int libroId, Comentario comentario);
+        Task UpdateComentarioAsync(Guid comentarioId,Comentario comentario);
+        Task DeleteComentarioAsync(Guid comentarioId);
+    }
+}
