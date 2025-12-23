5cfbeb25e02ed0ba6eec6fca5514e24e4f82ad7d
Pablo <ortegaperaltap@gmail.com>
2025-12-23 15:14:12 -0400

    creat DTO ComentarioPatch

---
+++ Biblioteca API/DTOs/ComentarioPatchDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Biblioteca_API.DTOs
+{
+    public class ComentarioPatchDTO
+    {
+        [StringLength(150, ErrorMessage = "El campo {0} debe tener {1} caracteres o menos")]
+        public required string Autor { get; set; }
+
+        [StringLength(500, ErrorMessage = "El campo {0} solo acepta {1} caracteres o menos")]
+        public required string Cuerpo { get; set; }
+        public DateTime FechaPublicacion { get; set; } = DateTime.UtcNow.Date;
+    }
+}
