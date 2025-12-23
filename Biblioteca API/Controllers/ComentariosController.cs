779f856ae6684658bff3179f44acfd0b7540474c
Pablo <ortegaperaltap@gmail.com>
2025-12-22 22:07:15 -0400

    cambiar ruta para que al querer obtener un comentario tiene que ingresar libro id en la ruta

--- Biblioteca API/Controllers/ComentariosController.cs
+++ Biblioteca API/Controllers/ComentariosController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Biblioteca_API.Datos.Repositorios;
+using Biblioteca_API.Entidades;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Biblioteca_API.Controllers
 {
     [ApiController]
-    [Route("/api/comentarios")]
+    [Route("api/libros/{libroId:int}/comentarios")]
     public class ComentariosController : ControllerBase
     {
     }
