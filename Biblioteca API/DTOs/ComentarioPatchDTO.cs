cc1cf938e16363e2bd81e49cf1ab8a4cd4df9a6c
Pablo <ortegaperaltap@gmail.com>
2025-12-23 16:34:21 -0400

    eliminar atributos Id, fechaPublicacion y autor

--- Biblioteca API/DTOs/ComentarioPatchDTO.cs
+++ Biblioteca API/DTOs/ComentarioPatchDTO.cs
@@ -4,11 +4,7 @@ namespace Biblioteca_API.DTOs
 {
     public class ComentarioPatchDTO
     {
-        [StringLength(150, ErrorMessage = "El campo {0} debe tener {1} caracteres o menos")]
-        public required string Autor { get; set; }
-
         [StringLength(500, ErrorMessage = "El campo {0} solo acepta {1} caracteres o menos")]
         public required string Cuerpo { get; set; }
-        public DateTime FechaPublicacion { get; set; } = DateTime.UtcNow.Date;
     }
 }
