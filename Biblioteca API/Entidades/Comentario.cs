6a32a0d1ef7adee1cea8a311b08a9df60a40cbc1
Pablo <ortegaperaltap@gmail.com>
2025-12-22 21:19:08 -0400

    agregar data annotations a atributos autor y cuerpo para limitar numero de caracteres

--- Biblioteca API/Entidades/Comentario.cs
+++ Biblioteca API/Entidades/Comentario.cs
@@ -1,9 +1,15 @@
-﻿namespace Biblioteca_API.Entidades
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Biblioteca_API.Entidades
 {
     public class Comentario
     {
         public Guid Id { get; set; }
+        [Required]
+        [StringLength(150, ErrorMessage = "El campo {0} debe tener {1} caracteres o menos")]
         public required string Autor { get; set; }
+        
+        [StringLength(500,ErrorMessage = "El campo {0} solo acepta {1} caracteres o menos")]
         public required string Cuerpo { get; set; }
         public DateTime FechaPublicacion { get; set; }
         public int LibroId { get; set; }
