2dd3f37eabfb7ce81cb4ec7dce24762fe6e16e2a
Pablo <ortegaperaltap@gmail.com>
2025-12-27 20:21:06 -0400

    crear DTO LibroCreacion

---
+++ Biblioteca API/DTOs/LibroCreacionDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Biblioteca_API.DTOs
+{
+    public class LibroCreacionDTO
+    {
+        [Required]
+        [StringLength(250, ErrorMessage = "El titulo del libro debe ser 250 caracteres o menos")]
+        public required string Titulo { get; set; }
+        [Required]
+        public required List<int> AutoresIds = [];
+    }
+}
