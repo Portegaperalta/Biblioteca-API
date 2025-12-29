284942eb7240ad8f2719918ce52481eec156a661
Pablo <ortegaperaltap@gmail.com>
2025-12-28 20:39:14 -0400

    Renombrar atributo "NombreAutores" a "Autores" y modificarla para que sea lista de Autores en vez de solo strings

--- Biblioteca API/DTOs/LibroConAutoresDTO.cs
+++ Biblioteca API/DTOs/LibroConAutoresDTO.cs
@@ -4,6 +4,6 @@ namespace Biblioteca_API.DTOs
 {
     public class LibroConAutoresDTO : LibroDTO
     {
-        public required List<string> NombreAutores { get; set; } = [];
+        public required List<Autor> Autores { get; set; } = [];
     }
 }
