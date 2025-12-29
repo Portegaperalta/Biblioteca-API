c46600fd49135c97132472d59d8687cf47045652
Pablo <ortegaperaltap@gmail.com>
2025-12-29 07:45:55 -0400

    refactorizar metodo abstracto MapLibrosToDto para que no sea sync y requiera parametro libros

--- Biblioteca API/Servicios/ILibroServicio.cs
+++ Biblioteca API/Servicios/ILibroServicio.cs
@@ -10,7 +10,7 @@ namespace Biblioteca_API.Servicios
         Task<LibroDTO> GetLibroDtoAsync(int libroId);
         Task CreateLibroAsync(LibroCreacionDTO libroCreacionDto);
         Task<int> DeleteLibroAsync(int libroId);
-        Task<IEnumerable<LibroDTO>> MapLibrosToDto();
+        List<LibroDTO> MapLibrosToDto(IEnumerable<Libro> libros);
         LibroDTO MapLibroToDto(Libro libro);
         Task<Libro> MapLibroCreacionDtoToLibro(LibroCreacionDTO libroCracionDto);
     }
