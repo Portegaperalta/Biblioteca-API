b169bb08f836c27b3ab188afaad97fdd76ccd7c9
Pablo <ortegaperaltap@gmail.com>
2025-12-28 21:16:32 -0400

    Crear folder Mappers para guardar mappers personalizados

---
+++ Biblioteca API/Mappers/LibroMapper.cs
@@ -0,0 +1,37 @@
+﻿using Biblioteca_API.DTOs;
+using Biblioteca_API.Entidades;
+
+namespace Biblioteca_API.Mappers
+{
+    public class LibroMapper
+    {
+        public LibroDTO? MapToDto(Libro libro)
+        {
+            if (libro is null)
+            {
+                return null;
+            }
+
+            return new LibroDTO
+            {
+                Id = libro.Id,
+                Titulo = libro.Titulo
+            };
+        }
+
+        public Libro? MapToEntity(LibroCreacionDTO libroCreacionDto)
+        {
+            if (libroCreacionDto is null)
+            {
+                return null;
+            }
+
+            return new Libro
+            {
+                Titulo = libroCreacionDto.Titulo,
+                Autores = libroCreacionDto.AutoresIds
+               .Select(id => new AutorLibro { AutorId = id }).ToList()
+            };
+        }
+    }
+}
