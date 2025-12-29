0541bafcd48890b9408b12a9c910eb45ceb37c31
Pablo <ortegaperaltap@gmail.com>
2025-12-28 21:49:50 -0400

    Crear interfaz con metodos abstractos para futura implementacion en LibroServicio

---
+++ Biblioteca API/Servicios/ILibroServicio.cs
@@ -0,0 +1,10 @@
+﻿using Biblioteca_API.DTOs;
+
+namespace Biblioteca_API.Servicios
+{
+    public interface ILibroServicio
+    {
+        Task<IEnumerable<LibroDTO>> MapLibrosToDto();
+        Task MapLibroCreacionDtoToLibro(LibroCreacionDTO libroCracionDto);
+    }
+}
