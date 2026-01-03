0bfe36cce9b4dc907550fb3987e0ba8dd7ed3b4b
Pablo <ortegaperaltap@gmail.com>
2026-01-03 10:17:56 -0400

    inyectar dependencia repositorioAutor

--- Biblioteca API/Servicios/AutorServicio.cs
+++ Biblioteca API/Servicios/AutorServicio.cs
@@ -1,6 +1,14 @@
-﻿namespace Biblioteca_API.Servicios
+﻿using Biblioteca_API.Datos.Repositorios;
+
+namespace Biblioteca_API.Servicios
 {
     public class AutorServicio : IAutorServicio
     {
+        private readonly IRepositorioAutor _repositorioAutor;
+
+        public AutorServicio(IRepositorioAutor repositorioAutor)
+        {
+            _repositorioAutor = repositorioAutor;
+        }
     }
 }
