0efe315a95f0ae3c8b4f1544cda4522c93d8026a
Pablo <ortegaperaltap@gmail.com>
2026-01-03 13:15:20 -0400

    crear metodo abstracto  HandleAutorPatchAsync

--- Biblioteca API/Servicios/IAutorServicio.cs
+++ Biblioteca API/Servicios/IAutorServicio.cs
@@ -1,5 +1,6 @@
 ﻿using Biblioteca_API.DTOs;
 using Biblioteca_API.Entidades;
+using Microsoft.AspNetCore.JsonPatch;
 
 namespace Biblioteca_API.Servicios
 {
@@ -11,6 +12,7 @@ namespace Biblioteca_API.Servicios
         Task<AutorSinLibrosDTO?> GetAutorSinLibrosDtoAsync(int autorId);
         Task CreateAutorAsync(AutorCreacionDTO autorCreacionDto);
         Task UpdateAutorAsync(AutorPutDTO autorPutDTO);
+        Task HandleAutorPatchAsync(int libroId, JsonPatchDocument<AutorPatchDTO> patchDoc);
         Task<int> DeleteAutorAsync(int autorId);
     }
 }
