43823e8e3cfe42978a8a3b46848ce5b38a29f631
Pablo <ortegaperaltap@gmail.com>
2026-01-03 00:30:34 -0400

    crear interfaz IAutorServicio

---
+++ Biblioteca API/Servicios/IAutorServicio.cs
@@ -0,0 +1,14 @@
+﻿using Biblioteca_API.DTOs;
+
+namespace Biblioteca_API.Servicios
+{
+    public interface IAutorServicio
+    {
+        Task<IEnumerable<AutorDTO>> GetAutoresDtoAsync();
+        Task<AutorDTO?> GetAutorDtoAsync(int autorId);
+        Task<AutorSinLibrosDTO> GetAutorSinLibrosDtoAsync(int autorId);
+        Task CreateAutorAsync(AutorCreacionDTO autorCreacionDto);
+        Task UpdateAutorAsync();
+        Task<int> DeleteAutorAsyc(int autorId);
+    }
+}
