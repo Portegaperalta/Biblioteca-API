aac6773ac478460b79241210f5b40b4a9934ad1e
Pablo <ortegaperaltap@gmail.com>
2026-01-03 10:47:00 -0400

    inyectar dependencia autorMapper en clase AutorServicio

--- Biblioteca API/Servicios/AutorServicio.cs
+++ Biblioteca API/Servicios/AutorServicio.cs
@@ -1,14 +1,17 @@
 ﻿using Biblioteca_API.Datos.Repositorios;
+using Biblioteca_API.Mappers;
 
 namespace Biblioteca_API.Servicios
 {
     public class AutorServicio : IAutorServicio
     {
         private readonly IRepositorioAutor _repositorioAutor;
+        private readonly AutorMapper _autorMapper;
 
-        public AutorServicio(IRepositorioAutor repositorioAutor)
+        public AutorServicio(IRepositorioAutor repositorioAutor,AutorMapper autorMapper)
         {
             _repositorioAutor = repositorioAutor;
+            _autorMapper = autorMapper;
         }
     }
 }
