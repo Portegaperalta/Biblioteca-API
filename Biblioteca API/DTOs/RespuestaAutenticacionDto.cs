0df11d903b393e8f8c0d6b43feb5cfa2a144fedb
Pablo <ortegaperaltap@gmail.com>
2026-01-05 13:43:20 -0400

    Crear DTO RespuestaAutenticacion

---
+++ Biblioteca API/DTOs/RespuestaAutenticacionDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Biblioteca_API.DTOs
+{
+    public class RespuestaAutenticacionDto
+    {
+        public required string Token { get; set; }
+        public DateTime Expiracion { get; set; }
+    }
+}
