beccdd4479f704fbed61571f84bf88a45b5f4a99
Pablo <ortegaperaltap@gmail.com>
2026-01-05 15:26:06 -0400

    agregar atributos AutorId y AutorEmail

--- Biblioteca API/DTOs/ComentarioDTO.cs
+++ Biblioteca API/DTOs/ComentarioDTO.cs
@@ -4,11 +4,15 @@ namespace Biblioteca_API.DTOs
 {
     public class ComentarioDTO
     {
+        public Guid Id { get; set; }
+
         [StringLength(150, ErrorMessage = "El campo {0} debe tener {1} caracteres o menos")]
         public required string Autor { get; set; }
 
         [StringLength(500, ErrorMessage = "El campo {0} solo acepta {1} caracteres o menos")]
         public required string Cuerpo { get; set; }
         public DateTime FechaPublicacion { get; set; } = DateTime.UtcNow.Date;
+        public required string AutorId { get; set; }
+        public required string AutorEmail { get; set; }
     }
 }
