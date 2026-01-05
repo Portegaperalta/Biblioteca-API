57d1f36d1fa58c118c06853660cd34f55c460fc1
Pablo <ortegaperaltap@gmail.com>
2026-01-05 13:22:42 -0400

    agregar servicios para configurar identity

--- Biblioteca API/Program.cs
+++ Biblioteca API/Program.cs
@@ -1,8 +1,11 @@
+using System.Text;
 using Biblioteca_API.Datos;
 using Biblioteca_API.Datos.Repositorios;
 using Biblioteca_API.Mappers;
 using Biblioteca_API.Servicios;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,8 +20,34 @@ builder.Services.AddScoped<ILibroServicio, LibroServicio>();
 builder.Services.AddScoped<IRepositorioAutor, RepositorioAutor>();
 builder.Services.AddScoped<IRepositorioLibro, RepositorioLibro>();
 builder.Services.AddScoped<IRepositorioComentario, RepositorioComentario>();
+
 builder.Services.AddDbContext<ApplicationDbContext>(opciones => 
 opciones.UseSqlServer("name=DefaultConnection"));
+
+builder.Services.AddIdentityCore<IdentityUser>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+builder.Services.AddScoped<UserManager<IdentityUser>>();
+builder.Services.AddScoped<SignInManager<IdentityUser>>();
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddAuthentication().AddJwtBearer(opciones =>
+{
+    opciones.MapInboundClaims = false;
+
+    opciones.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = 
+        new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["llaveJwt"]!)),
+        ClockSkew = TimeSpan.Zero
+    };
+
+});
 
 var app = builder.Build();
 
