be01f14338135fa4d66e7e230a6c96ca3739006f
Pablo <ortegaperaltap@gmail.com>
2026-01-05 15:32:08 -0400

    renombrar atributos relacionados relacionados al usuario

--- Biblioteca API/DTOs/ComentarioDTO.cs
+++ Biblioteca API/DTOs/ComentarioDTO.cs
@@ -7,12 +7,12 @@ namespace Biblioteca_API.DTOs
         public Guid Id { get; set; }
 
         [StringLength(150, ErrorMessage = "El campo {0} debe tener {1} caracteres o menos")]
-        public required string Autor { get; set; }
+        public required string Usuario { get; set; }
 
         [StringLength(500, ErrorMessage = "El campo {0} solo acepta {1} caracteres o menos")]
         public required string Cuerpo { get; set; }
         public DateTime FechaPublicacion { get; set; } = DateTime.UtcNow.Date;
-        public required string AutorId { get; set; }
-        public required string AutorEmail { get; set; }
+        public required string UsuarioId { get; set; }
+        public required string UsuarioEmail { get; set; }
     }
 }
