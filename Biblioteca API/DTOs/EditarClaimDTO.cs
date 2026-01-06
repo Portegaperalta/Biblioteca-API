e5990d01c879d05148908589c29d26f94eb7d39a
Pablo <ortegaperaltap@gmail.com>
2026-01-05 23:35:58 -0400

    crear DTO EditarClaim

---
+++ Biblioteca API/DTOs/EditarClaimDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Biblioteca_API.DTOs
+{
+    public class EditarClaimDTO
+    {
+        [Required]
+        [EmailAddress]
+       public required string Email { get; set; }
+    }
+}
