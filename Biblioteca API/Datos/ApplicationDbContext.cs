95441a0f96c6ab89a6cf5921c8276515b108499f
Pablo <ortegaperaltap@gmail.com>
2026-01-06 21:39:52 -0400

    especificar que clase personalizada Usuario es la que representa el tipo de dato Usuario

--- Biblioteca API/Datos/ApplicationDbContext.cs
+++ Biblioteca API/Datos/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Biblioteca_API.Datos
 {
-    public class ApplicationDbContext: IdentityDbContext
+    public class ApplicationDbContext: IdentityDbContext<Usuario>
     {
         public ApplicationDbContext(DbContextOptions options) : base (options)
         {
