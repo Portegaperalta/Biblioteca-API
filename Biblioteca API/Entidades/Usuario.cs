21adcb2c1aaba5862ab4ed3a68126839744f3b2b
Pablo <ortegaperaltap@gmail.com>
2026-01-06 21:35:55 -0400

    crear clase Usuario para agregar atributo  personalizado FechaNacimiento

---
+++ Biblioteca API/Entidades/Usuario.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Biblioteca_API.Entidades
+{
+    public class Usuario : IdentityUser
+    {
+        public DateTime FechaNacimiento { get; set; }
+    }
+}
