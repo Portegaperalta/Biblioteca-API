950e6b992bb789f28617d35149b292ae847f9dab
Pablo <ortegaperaltap@gmail.com>
2026-01-06 22:16:10 -0400

    crear DTO Usuario

---
+++ Biblioteca API/DTOs/UsuarioDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Biblioteca_API.DTOs
+{
+    public class UsuarioDTO
+    {
+        public required string Email { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+    }
+}
