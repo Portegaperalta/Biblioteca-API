7abb1ea75ea204c09df7f59349eb6c812e745f55
Pablo <ortegaperaltap@gmail.com>
2026-01-06 22:20:39 -0400

    Crear mapper para clase Usuario con metodo MapToDto

---
+++ Biblioteca API/Mappers/UsuarioMapper.cs
@@ -0,0 +1,17 @@
+﻿using Biblioteca_API.DTOs;
+using Biblioteca_API.Entidades;
+
+namespace Biblioteca_API.Mappers
+{
+    public class UsuarioMapper
+    {
+        public UsuarioDTO MapToDto(Usuario usuario)
+        {
+            return new UsuarioDTO
+            {
+             Email = usuario.Email!,
+             FechaNacimiento = usuario.FechaNacimiento
+            };
+        }
+    }
+}
