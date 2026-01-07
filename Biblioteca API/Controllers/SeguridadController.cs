b643f7710ab3b015c7f0aa2d5009e9dc8910ab36
Pablo <ortegaperaltap@gmail.com>
2026-01-07 08:45:25 -0400

    crear SeguridadController y crear acciones para Encriptar y Desencriptar

---
+++ Biblioteca API/Controllers/SeguridadController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Biblioteca_API.Controllers
+{
+    [Route("api/seguridad")]
+    [ApiController]
+    public class SeguridadController : ControllerBase
+    {
+  
+        private readonly IDataProtector _protector;
+        public SeguridadController(IDataProtectionProvider dataProtectionProvider)
+        {
+            _protector = dataProtectionProvider.CreateProtector("SeguridadController");
+        }
+
+        [HttpGet("encriptar")]
+        public ActionResult Encriptar([FromQuery]string textoPlano)
+        {
+            string textoCifrado = _protector.Protect(textoPlano);
+            return Ok(new { textoCifrado });
+        }
+
+        [HttpGet("desencriptar")]
+        public ActionResult Desencriptar(string textoCifrado)
+        {
+            string textoPlano = _protector.Unprotect(textoCifrado);
+            return Ok(new {textoPlano});
+        }
+    }
+}
