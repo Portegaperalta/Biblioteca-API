925d0d3b7bc715ed7ed8f2369395656d34f182fd
Pablo <ortegaperaltap@gmail.com>
2026-01-08 22:15:23 -0400

    agregar parametro PaginacionDTO  a metodo en controller y AutorServicio relacionados con obtener listado de autores

--- Biblioteca API/Servicios/AutorServicio.cs
+++ Biblioteca API/Servicios/AutorServicio.cs
@@ -18,7 +18,7 @@ namespace Biblioteca_API.Servicios
             _autorMapper = autorMapper;
         }
 
-        public async Task<IEnumerable<AutorDTO>> GetAutoresDtoAsync()
+        public async Task<IEnumerable<AutorDTO>> GetAutoresDtoAsync(PaginacionDTO paginacionDTO)
         {
             var autores = await _repositorioAutor.GetAutoresAsync();
             var autoresDto = autores.Select(autor => _autorMapper.MapToAutorDto(autor));
