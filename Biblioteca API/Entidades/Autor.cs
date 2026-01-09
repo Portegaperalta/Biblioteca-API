be8423a35490d07ccc948e362e17475e685817b4
Pablo <ortegaperaltap@gmail.com>
2026-01-08 22:39:16 -0400

    agregar atributo Foto para poder guardar url de una imagen

--- Biblioteca API/Entidades/Autor.cs
+++ Biblioteca API/Entidades/Autor.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using Biblioteca_API.Validaciones;
+using Microsoft.EntityFrameworkCore;
 
 namespace Biblioteca_API.Entidades
 {
@@ -19,7 +20,8 @@ namespace Biblioteca_API.Entidades
 
         [StringLength(20, ErrorMessage = "El campo {0} debe tener {1} caracteres o menos")]
         public string? Identificacion { get; set; }
-
+        [Unicode(false)]
+        public string? Foto { get; set; }
         public List<AutorLibro> Libros { get; set; } = [];
         }
 }
