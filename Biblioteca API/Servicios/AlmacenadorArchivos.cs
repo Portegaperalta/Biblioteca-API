c8d4d3765f08c744518786949b47c1484846d0cf
Pablo <ortegaperaltap@gmail.com>
2026-01-09 17:01:43 -0400

    .

--- Biblioteca API/Servicios/AlmacenadorArchivos.cs
+++ Biblioteca API/Servicios/AlmacenadorArchivos.cs
@@ -1,8 +1,14 @@
 ﻿
+
 namespace Biblioteca_API.Servicios
 {
     public class AlmacenadorArchivos : IAlmacenadorArchivos
     {
+        public Task<string> Almacenar(string contenedor, IFormFile archivo)
+        {
+            throw new NotImplementedException();
+        }
+
         public Task Borrar(string? ruta, string contenedor)
         {
             throw new NotImplementedException();
