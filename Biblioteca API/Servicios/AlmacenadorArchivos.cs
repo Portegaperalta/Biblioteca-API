74914c2fe13917ba131741ecb23836584330ad00
Pablo <ortegaperaltap@gmail.com>
2026-01-08 23:03:20 -0400

    crear servicio AlmacenadorArchivo con metodos para almacenar, borrar y editar

---
+++ Biblioteca API/Servicios/AlmacenadorArchivos.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Biblioteca_API.Servicios
+{
+    public class AlmacenadorArchivos : IAlmacenadorArchivos
+    {
+        public Task Borrar(string? ruta, string contenedor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
