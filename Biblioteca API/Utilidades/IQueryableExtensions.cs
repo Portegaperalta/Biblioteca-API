217d9f0f0049761b7d1abe78031024da36bf59bf
Pablo <ortegaperaltap@gmail.com>
2026-01-10 18:04:30 -0400

    crear folder de utilidades con utilidad IQueryableExtensions

---
+++ Biblioteca API/Utilidades/IQueryableExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Biblioteca_API.DTOs;
+
+namespace Biblioteca_API.Utilidades
+{
+    public static class IQueryableExtensions
+    {
+        public static IQueryable<T> Paginar<T>(this IQueryable<T> queryable,
+            PaginacionDTO paginacionDTO)
+        {
+            return queryable.Skip((paginacionDTO.Pagina) - 1 * paginacionDTO.RecordsPorPagina)
+                            .Take(paginacionDTO.RecordsPorPagina);
+        }
+    }
+}
