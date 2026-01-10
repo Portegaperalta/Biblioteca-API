b04dfcde9baebb52400c441f8b6cf3b720d0fc47
Pablo <ortegaperaltap@gmail.com>
2026-01-10 11:39:12 -0400

    cambiar el argumento libroPutDTO en endpoint PUT para que sea FromForm en vez de fromBody

--- Biblioteca API/Controllers/LibrosController.cs
+++ Biblioteca API/Controllers/LibrosController.cs
@@ -80,7 +80,7 @@ namespace Biblioteca_API.Controllers
         [EndpointDescription("Actualiza libro por ID, si el ID del libro en la ruta no coincide con ID de libro de peticion, devuelve status 400 (Bad Request)")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<ActionResult> Put([FromRoute]int id,[FromBody] LibroPutDTO libroPutDto)
+        public async Task<ActionResult> Put([FromRoute]int id,[FromForm] LibroPutDTO libroPutDto)
         {
             if (id != libroPutDto.Id)
             {
