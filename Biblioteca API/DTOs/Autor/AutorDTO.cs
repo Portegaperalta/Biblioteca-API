8d483aa12cf95b911a77a048731f61afbede3b6c
Pablo <ortegaperaltap@gmail.com>
2026-01-13 23:49:15 -0400

    hacer AutorDTO clase hija de RecursoDTO

--- Biblioteca API/DTOs/Autor/AutorDTO.cs
+++ Biblioteca API/DTOs/Autor/AutorDTO.cs
@@ -1,9 +1,10 @@
-﻿using Biblioteca_API.DTOs.Libro;
+﻿using Biblioteca_API.DTOs.HATEOAS;
+using Biblioteca_API.DTOs.Libro;
 using Biblioteca_API.Entidades;
 
 namespace Biblioteca_API.DTOs.Autor
 {
-    public class AutorDTO
+    public class AutorDTO : RecursoDTO
     {
         public int Id { get; set; }
         public required string NombreCompleto { get; set; }
