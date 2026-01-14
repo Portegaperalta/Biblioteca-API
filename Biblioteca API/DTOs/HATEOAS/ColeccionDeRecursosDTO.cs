5b0615142c5e00a935b490d1bfdd0a616e835d88
Pablo <ortegaperaltap@gmail.com>
2026-01-14 07:59:06 -0400

    crear clase ColeccionDeRecursosDTO

---
+++ Biblioteca API/DTOs/HATEOAS/ColeccionDeRecursosDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace Biblioteca_API.DTOs.HATEOAS
+{
+    public class ColeccionDeRecursosDTO<T>: RecursoDTO where T: RecursoDTO
+    {
+        public IEnumerable<T> Valores { get; set; } = [];
+    }
+}
