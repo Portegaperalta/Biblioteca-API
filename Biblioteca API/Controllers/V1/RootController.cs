6468dc9c0bc88f9775cc794cd664c48d937e15e7
Pablo <ortegaperaltap@gmail.com>
2026-01-13 23:42:30 -0400

    agregar validacion de si usuario es admin para mostrar rutas permitidas dependiendo de los permisos del usuario

--- Biblioteca API/Controllers/V1/RootController.cs
+++ Biblioteca API/Controllers/V1/RootController.cs
@@ -1,16 +1,31 @@
-﻿using Biblioteca_API.DTOs.HATEOAS;
+﻿using System.Threading.Tasks;
+using Biblioteca_API.DTOs.HATEOAS;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Biblioteca_API.Controllers.V1
 {
     [ApiController]
     [Route("api/v1")]
+    [Authorize]
     public class RootController : ControllerBase
     {
+        private readonly IAuthorizationService _authorizationService;
+
+        public RootController(IAuthorizationService authorizationService)
+        {
+            _authorizationService = authorizationService;
+        }
+
         [HttpGet("ObtenerRootV1",Name = "ObtenerRootV1")]
-        public IEnumerable<DatosHATEOASDTO> Get()
+        [AllowAnonymous]
+        public async Task<IEnumerable<DatosHATEOASDTO>> Get()
         {
             var datosHateoas = new List<DatosHATEOASDTO>();
+
+            var esAdmin = await _authorizationService.AuthorizeAsync(User, "esAdmin");
+
+            //Acciones que cualquiera puede realizar
 
             datosHateoas.Add(new DatosHATEOASDTO(
                                  Enlace: Url.Link("ObtenerRootV1", new {})!,
@@ -23,19 +38,9 @@ namespace Biblioteca_API.Controllers.V1
                 Metodo:"GET"));
 
             datosHateoas.Add(new DatosHATEOASDTO(
-                Enlace: Url.Link("CrearAutorV1", new { })!,
-                Descripcion: "autor-crear",
-                Metodo: "POST"));
-
-            datosHateoas.Add(new DatosHATEOASDTO(
                 Enlace: Url.Link("ObtenerLibrosV1", new { })!,
                 Descripcion: "libros-obtener",
                 Metodo: "GET"));
-
-            datosHateoas.Add(new DatosHATEOASDTO(
-                Enlace: Url.Link("CrearLibroV1", new { })!,
-                Descripcion: "libro-crear",
-                Metodo: "POST"));
 
             datosHateoas.Add(new DatosHATEOASDTO(
                 Enlace: Url.Link("ObtenerComentariosV1", new {libroId = 1 })!,
@@ -46,11 +51,6 @@ namespace Biblioteca_API.Controllers.V1
                 Enlace: Url.Link("CrearComentarioV1", new { })!,
                 Descripcion: "comentario-crear",
                 Metodo: "POST"));
-
-            datosHateoas.Add(new DatosHATEOASDTO(
-                Enlace: Url.Link("ObtenerUsuariosV1", new { })!,
-                Descripcion: "usuarios-obtener",
-                Metodo: "GET"));
 
             datosHateoas.Add(new DatosHATEOASDTO(
                 Enlace: Url.Link("RegistrarUsuarioV1", new { })!,
@@ -66,6 +66,26 @@ namespace Biblioteca_API.Controllers.V1
                 Enlace: Url.Link("RenovarTokenV1", new { })!,
                 Descripcion: "token-renovar",
                 Metodo: "GET"));
+
+            //Acciones que solo usuarios admin puede realizar
+
+            if (esAdmin.Succeeded)
+            {
+                datosHateoas.Add(new DatosHATEOASDTO(
+                Enlace: Url.Link("CrearAutorV1", new { })!,
+                Descripcion: "autor-crear",
+                Metodo: "POST"));
+
+                datosHateoas.Add(new DatosHATEOASDTO(
+                Enlace: Url.Link("CrearLibroV1", new { })!,
+                Descripcion: "libro-crear",
+                Metodo: "POST"));
+
+                datosHateoas.Add(new DatosHATEOASDTO(
+                Enlace: Url.Link("ObtenerUsuariosV1", new { })!,
+                Descripcion: "usuarios-obtener",
+                Metodo: "GET"));
+            }
 
             datosHateoas.Add(new DatosHATEOASDTO(
                 Enlace: Url.Link("HacerAdminV1", new { })!,
