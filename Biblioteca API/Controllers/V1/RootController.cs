d47d97635e9bfdd0d6e3405ebfb990fcebf4f3b8
Pablo <ortegaperaltap@gmail.com>
2026-01-13 23:10:53 -0400

    agregar rutas de consulta y creacion de autores, libros y comentarios controllers

--- Biblioteca API/Controllers/V1/RootController.cs
+++ Biblioteca API/Controllers/V1/RootController.cs
@@ -17,6 +17,35 @@ namespace Biblioteca_API.Controllers.V1
                                  Descripcion: "self",
                                  Metodo:"GET"));
 
+            datosHateoas.Add(new DatosHATEOASDTO(
+                Enlace: Url.Link("ObtenerAutoresV1",new {})!,
+                Descripcion:"autores-obtener",
+                Metodo:"GET"));
+
+            datosHateoas.Add(new DatosHATEOASDTO(
+                Enlace: Url.Link("CrearAutorV1", new { })!,
+                Descripcion: "autor-crear",
+                Metodo: "POST"));
+
+            datosHateoas.Add(new DatosHATEOASDTO(
+                Enlace: Url.Link("ObtenerLibrosV1", new { })!,
+                Descripcion: "libros-obtener",
+                Metodo: "GET"));
+
+            datosHateoas.Add(new DatosHATEOASDTO(
+                Enlace: Url.Link("CrearLibroV1", new { })!,
+                Descripcion: "libro-crear",
+                Metodo: "POST"));
+
+            datosHateoas.Add(new DatosHATEOASDTO(
+                Enlace: Url.Link("ObtenerComentariosV1", new { })!,
+                Descripcion: "comentarios-obtener",
+                Metodo: "GET"));
+
+            datosHateoas.Add(new DatosHATEOASDTO(
+                Enlace: Url.Link("CrearComentarioV1", new { })!,
+                Descripcion: "comentario-crear",
+                Metodo: "POST"));
             return datosHateoas;
         }
     }
