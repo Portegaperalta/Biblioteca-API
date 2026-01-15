311c501c3f5a73e6a5fcd5566c685bee20547b3f
Pablo <ortegaperaltap@gmail.com>
2026-01-14 23:31:48 -0400

    crear helpers de construccion y metodo  GetAutorDtoAsync_RetornarNull_CuandoAutorDesdeBDEsNull para testear si retorna nulo al ingresar autorId que no existe

--- BibliotecaAPITests/PruebasUnitarias/Servicios/AutorServicioPruebas.cs
+++ BibliotecaAPITests/PruebasUnitarias/Servicios/AutorServicioPruebas.cs
@@ -1,14 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Biblioteca_API.Datos;
+using Biblioteca_API.Datos.Repositorios;
+using Biblioteca_API.Mappers;
+using Biblioteca_API.Servicios;
+using BibliotecaAPITests.Utilidades;
+using Microsoft.Extensions.Logging;
 
 namespace BibliotecaAPITests.PruebasUnitarias.Servicios
 {
     [TestClass]
-    public class AutorServicioPruebas 
+    public class AutorServicioPruebas : BasePruebas
     {
         // helpers de construccion
+        protected RepositorioAutor ConstruirRepositorioAutor(ApplicationDbContext context)
+        {
+            return new RepositorioAutor(context);
+        }
+
+        protected AutorServicio ConstruirAutorServicio(IRepositorioAutor repositorioAutor, AutorMapper autorMapper,
+                             IAlmacenadorArchivos almacenadorArchivos, ILogger<AutorServicio> logger)
+        {
+            return new AutorServicio(repositorioAutor, autorMapper, almacenadorArchivos, logger);
+        }
+
+        protected AutorMapper ConstruirMapper()
+        {
+            return new AutorMapper();
+        }
+
+        [TestMethod]
+        [DataRow(1)]
+        public async Task GetAutorDtoAsync_RetornarNull_CuandoAutorDesdeBDEsNull(int autorId)
+        {
+            //Preparacion
+            var nombreDB = Guid.NewGuid().ToString();
+            var context = ConstruirContext(nombreDB);
+            var repositorioAutor = ConstruirRepositorioAutor(context);
+            var autorMapper = ConstruirMapper();
+            IAlmacenadorArchivos almacenadorArchivos = null!;
+            ILogger<AutorServicio> logger = null!;
+
+            var autorServicio = new AutorServicio(repositorioAutor, autorMapper,almacenadorArchivos, logger);
+
+            //Prueba
+            var resultado = await autorServicio.GetAutorDtoAsync(autorId);
+
+            //Validacion
+            Assert.AreEqual(expected: null, actual: resultado);
+        }
     }
 }
