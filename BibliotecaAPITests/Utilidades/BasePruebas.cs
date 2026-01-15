e9c0fefa44934d0989fd2d895a1e96a8d879cd60
Pablo <ortegaperaltap@gmail.com>
2026-01-14 21:42:54 -0400

    crear clase BasePruebas para poder instanciar un dbcontet en cualquier prueba que herede de dicha clase

---
+++ BibliotecaAPITests/Utilidades/BasePruebas.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Biblioteca_API.Datos;
+using Microsoft.EntityFrameworkCore;
+
+namespace BibliotecaAPITests.Utilidades
+{
+    public class BasePruebas
+    {
+        protected ApplicationDbContext ConstruirContext(string nombreBD)
+        {
+            var opcioones = new DbContextOptionsBuilder<ApplicationDbContext>()
+                                .UseInMemoryDatabase(nombreBD).Options;
+
+            var dbContext = new ApplicationDbContext(opcioones);
+            return dbContext;
+        }
+    }
+}
