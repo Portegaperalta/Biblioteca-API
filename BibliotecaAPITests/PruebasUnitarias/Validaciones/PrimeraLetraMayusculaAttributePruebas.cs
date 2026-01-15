4018b35ef941f7fb4514f855913fc5e30642509e
Pablo <ortegaperaltap@gmail.com>
2026-01-14 20:47:05 -0400

    crear metodo para testear IsValid en casa de que la primera letra sea mayuscula devuelva success

--- BibliotecaAPITests/PruebasUnitarias/Validaciones/PrimeraLetraMayusculaAttributePruebas.cs
+++ BibliotecaAPITests/PruebasUnitarias/Validaciones/PrimeraLetraMayusculaAttributePruebas.cs
@@ -41,5 +41,21 @@ namespace BibliotecaAPITests.PruebasUnitarias.Validaciones
             //Verificacion
             Assert.AreEqual(expected: "La primera letra debe ser Mayuscula", resultado!.ErrorMessage);
         }
+
+        [TestMethod]
+        public void IsValid_RetornaExitoso_siPrimeraLetraMayuscula()
+        {
+            //Preparacion
+            var primeraLetraMayusculaAttribute = new PrimeraLetraMayusculaAttribute();
+            var validationContext = new ValidationContext(new object { });
+            var text = "Text";
+            var value = text[0];
+
+            //Prueba
+            var resultado = primeraLetraMayusculaAttribute.GetValidationResult(value, validationContext);
+
+            //Verificacion
+            Assert.AreEqual(expected: ValidationResult.Success, actual: resultado);
+        }
     }
 }
