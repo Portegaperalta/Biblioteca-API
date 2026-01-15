183b3fd031260e788f92ca1f0cc1fcdfc52a19c2
Pablo <ortegaperaltap@gmail.com>
2026-01-14 20:44:30 -0400

    agregar metodo para testear IsValid si la primera letra no es mayuscula

---
+++ BibliotecaAPITests/PruebasUnitarias/Validaciones/PrimeraLetraMayusculaAttributePruebas.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Biblioteca_API.Validaciones;
+
+namespace BibliotecaAPITests.PruebasUnitarias.Validaciones
+{
+    [TestClass]
+    public class PrimeraLetraMayusculaAttributePruebas
+    {
+        [TestMethod]
+        public void IsValid_RetornaExitoso_siValueEsVacio()
+        {
+            //Preparacion
+            var primeraLetraMayusculaAttribute = new PrimeraLetraMayusculaAttribute();
+            var validationContext = new ValidationContext(new object { });
+            var value = string.Empty;
+
+            //Prueba
+            var resultado = primeraLetraMayusculaAttribute.GetValidationResult(value, validationContext);
+
+            //Verificacion
+            Assert.AreEqual(expected: ValidationResult.Success, actual: resultado);
+        }
+
+        [TestMethod]
+        public void IsValid_RetornaValidationResult_siPrimeraLetraNoMayuscula()
+        {
+            //Preparacion
+            var primeraLetraMayusculaAttribute = new PrimeraLetraMayusculaAttribute();
+            var validationContext = new ValidationContext(new object { });
+            var text = "text";
+            var value = text[0];
+
+            //Prueba
+            var resultado = primeraLetraMayusculaAttribute.GetValidationResult(value, validationContext);
+
+            //Verificacion
+            Assert.AreEqual(expected: "La primera letra debe ser Mayuscula", resultado!.ErrorMessage);
+        }
+    }
+}
