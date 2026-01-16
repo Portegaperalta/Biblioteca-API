0d2c7da4b50f6f84095fd2bc1ed0e02176448441
Pablo <ortegaperaltap@gmail.com>
2026-01-16 15:31:20 -0400

    crear testMethod para validar si accion PUT retorna 400 cuando ids no coinciden

--- BibliotecaAPITests/PruebasUnitarias/Controllers/V1/AutoresControllerPruebas.cs
+++ BibliotecaAPITests/PruebasUnitarias/Controllers/V1/AutoresControllerPruebas.cs
@@ -188,5 +188,38 @@ namespace BibliotecaAPITests.PruebasUnitarias.Controllers.V1
             //Validacion
             await autorServicio.Received(1).CreateAutorAsync(autorCreacionDTO);
         }
+
+        //PUT
+        [TestMethod]
+        [DataRow(1)]
+        public async Task Put_Retorna400_CuandoAutorIdDeRutaNoEsIgualAIdDeAutorPutDTO(int autorIdFromRoute)
+        {
+            //Preparacion
+            var nombreDB = Guid.NewGuid().ToString();
+            var context = ConstruirContext(nombreDB);
+            var repositorioAutor = ConstruirRepositorioAutor(context);
+            var autorMapper = ConstruirMapper();
+            IAlmacenadorArchivos almacenadorArchivos = Substitute.For<IAlmacenadorArchivos>();
+            ILogger<AutorServicio> logger = Substitute.For<ILogger<AutorServicio>>();
+            IOutputCacheStore outputCacheStore = Substitute.For<IOutputCacheStore>();
+            IAutorServicio autorServicio = Substitute.For<IAutorServicio>();
+
+            var autoresController = new AutoresController(autorServicio, outputCacheStore);
+
+            var autorPutDTO = new AutorPutDTO
+            {
+             Id = 2,
+             Nombres = "William",
+             Apellidos = "Shakespeare",
+             Identificacion = "456"
+            };
+
+            //Prueba
+            var respuesta = await autoresController.Put(autorIdFromRoute, autorPutDTO);
+
+            //Validacion
+            var resultado = respuesta as BadRequestObjectResult;
+            Assert.AreEqual(expected: 400, actual: resultado!.StatusCode);
+        }
     }
 }
